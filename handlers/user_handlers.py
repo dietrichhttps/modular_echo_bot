a8d55a6fdea61f3c92b435b6ea32ea9b197d4e19
Alice Glass <abayhanov1998@gmail.com>
2024-02-14 21:46:14 +0300

    Добавил класс Router и задекорировал объектом класса Router хэндлеры

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,14 +1,17 @@
+from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import Command, CommandStart
 
 from lexicon.lexicon import LEXICON_RU
 
+router = Router()
 
-@dp.message(CommandStart())
+
+@router.message(CommandStart())
 async def process_start_command(message: Message):
     await message.answer(text=LEXICON_RU['/start'])
 
 
-@dp.message(Command('/help'))
+@router.message(Command('/help'))
 async def procces_help_command(message: Message):
     await message.answer(text=LEXICON_RU['/help'])
